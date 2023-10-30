58c4994d4b9914193e8736f620c913fa2d6c5da7
tushary <tushar.yadav@amdocs.com>
2023-10-31 02:58:02 +0530

    adding images to hero section and adding mask for dull background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+
 
 @NgModule({
   declarations: [
--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.scss']
+  styleUrls: ['./hero-section.component.scss'],
+  
 })
-export class HeroSectionComponent {
+export class HeroSectionComponent implements AfterViewInit {
+
+  images: string[] = [
+    '../../assets/img/2.jpg',
+    '../../assets/img/3.jpg',
+    '../../assets/img/4.jpg',
+    '../../assets/img/5.jpg',
+    '../../assets/img/1.jpg',
+    
+  ];
+  _currentImage = 0;
+
+  public get currentImageUrl() {
+    return this.images[this._currentImage];
+  }
+
+  ngAfterViewInit(): void {
+    this.startAutoPlay();
+  }
+
+  startAutoPlay() {
+    setInterval(() => {
+      this._currentImage = (this._currentImage + 1) % this.images.length;
+    }, 10000); // Change images every 3 seconds
+  }
 
 }
