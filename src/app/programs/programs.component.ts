174db37a654aa6dfaaa99629842f44e71b360855
tushary <tushar.yadav@amdocs.com>
2023-12-02 18:30:50 +0530

    modifying programs to meet the customer needs

--- src/app/programs/programs.component.ts
+++ src/app/programs/programs.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./programs.component.scss']
 })
 export class ProgramsComponent {
-  starter = [
-    "Personalized 1-on-1 private lessons",
-    "Tailored instruction for all skill levels",
-    "Fundamental grasp for beginners",
-    "Fine-tuning and strategic insights for advanced players",
-    "Dedicated coaches committed to your goals",
-    "Elevate your game to new heights"
-  ];
-
-  semiPrivate = [
-    "Dive into dynamic semi-private tennis lessons.",
-    "Bring a partner for an enjoyable learning experience.",
-    "Experience a lively and engaging tennis atmosphere.",
-    "Foster healthy competition and boost each other's skills.",
-    "Learn and have a blast on the court with a companion.",
-    "Motivate and excel together in our collaborative sessions."
-  ];
+ 
 }
